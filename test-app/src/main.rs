cef402b7223991ae2a9bf09b9b10a29cabd8e768
YdrMaster <ydrml@hotmail.com>
2022-10-29 12:23:22 +0800

    test: 测试直接调用或内核异常进入陷入流
    Signed-off-by: YdrMaster <ydrml@hotmail.com>

--- test-app/src/main.rs
+++ test-app/src/main.rs
@@ -4,8 +4,11 @@
 #![deny(warnings)]
 
 use console::log;
-use core::{arch::asm, mem::forget, ptr::NonNull};
-use fast_trap::{trap_entry, FastContext, FastResult, FlowContext, FreeTrapStack, TrapStackBlock};
+use core::{arch::asm, ptr::NonNull};
+use fast_trap::{
+    load_direct_trap_entry, trap_entry, FastContext, FastResult, FlowContext, FreeTrapStack,
+    TrapStackBlock,
+};
 use riscv::register::*;
 use sbi_rt::*;
 
@@ -60,21 +63,49 @@ extern "C" fn rust_main() {
     console::init_console(&Console);
     console::set_log_level(option_env!("LOG"));
     console::test_log();
+
     sscratch::write(0x5050);
     let context_ptr = unsafe { NonNull::new_unchecked(&mut ROOT_CONTEXT) };
+
+    // 测试构造和释放
     let _ = FreeTrapStack::new(unsafe { &mut ROOT_STACK }, context_ptr, fast_handler).unwrap();
     assert_eq!(0x5050, sscratch::read());
+
+    // 测试加载和卸载
     let _ = FreeTrapStack::new(unsafe { &mut ROOT_STACK }, context_ptr, fast_handler)
         .unwrap()
         .load();
     assert_eq!(0x5050, sscratch::read());
-    forget(
-        FreeTrapStack::new(unsafe { &mut ROOT_STACK }, context_ptr, fast_handler)
-            .unwrap()
-            .load(),
-    );
+
+    // 加载一个新的陷入栈
+    let _loaded = FreeTrapStack::new(unsafe { &mut ROOT_STACK }, context_ptr, fast_handler)
+        .unwrap()
+        .load();
     assert_ne!(0x5050, sscratch::read());
     log::debug!("sscratch: {:#x}", sscratch::read());
+
+    // 测试直接调用
+    unsafe {
+        asm!(
+            "   la   {0}, 1f
+                csrw sepc, {0}
+                li   {0}, 31
+                csrw scause, {0}
+                j    {trap}
+             1:
+            ",
+            out(reg) _,
+            trap = sym trap_entry,
+        );
+    }
+
+    // 测试内核异常
+    unsafe {
+        load_direct_trap_entry();
+        asm!("unimp");
+    }
+
+    loop {}
 }
 
 extern "C" fn fast_handler(
@@ -87,7 +118,17 @@ extern "C" fn fast_handler(
     a6: usize,
     a7: usize,
 ) -> FastResult {
-    log::debug!("fast trap!");
+    use {scause::Exception as E, scause::Trap as T};
+    let cause = scause::read();
+    match cause.cause() {
+        T::Exception(E::IllegalInstruction) => {
+            let mut sepc = sepc::read();
+            sepc += 2;
+            sepc::write(sepc);
+        }
+        T::Exception(_) | T::Interrupt(_) => {}
+    }
+    log::debug!("fast trap: {:?}({})", cause.cause(), cause.bits());
     ctx.save_args(a1, a2, a3, a4, a5, a6, a7);
     unsafe { sstatus::set_spp(sstatus::SPP::Supervisor) };
     ctx.restore()
